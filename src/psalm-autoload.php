bf2ee764077fe529f6d0b6d20419dbf7fb1fe44a
Torsten Simon <anfragen@tsgames.de>
2025-10-22 23:19:52 +0200

    psalm

--- src/psalm-autoload.php
+++ src/psalm-autoload.php
@@ -1,9 +1,11 @@
 <?php
 
-class SQLite3 {
-        public function __construct(string $filename) {}
+if(!class_exists('SQLite3')) {
+    class SQLite3 {
+            public function __construct(string $filename) {}
+    }
+    class SQLite3Result {}
 }
-class SQLite3Result {}
  require_once __DIR__ . "/config.php";
  require_once __DIR__ . "/static.php";
  require_once __DIR__ . "/functions.php";
